a0bff3bb5f42d9cbbfa9d1eeed901a1e3c9ccca7
Ray0Emma <ahmadifarah22@gmail.com>
2022-04-24 07:18:10 +0000

    feat: fixed employes ui

--- resources/views/livewire/employes.blade.php
+++ resources/views/livewire/employes.blade.php
@@ -5,47 +5,56 @@
             {{ __('Employes') }}
         </h2>
     </x-slot>
-    @if (Auth::user()->role == 'editor' || Auth::user()->role == 'admin')
-        @if ($isOpen)
-            @include('livewire.employe.update')
-        @else
-            @include('livewire.employe.create')
+    <div class="container mx-auto">
+        @if (Auth::user()->role == 'editor' || Auth::user()->role == 'admin')
+            @if ($isOpen)
+                @include('livewire.employe.update')
+            @else
+                @include('livewire.employe.create')
+            @endif
         @endif
-    @endif
-    <table class="table table-bordered mt-5" id="sampleTable">
-        <thead>
-            <tr>
-                <th>Emp_nss</th>
-                <th>Emp_nom</th>
-                <th>Emp_prn</th>
-                <th>Emp_tarif</th>
-                @if (Auth::user()->role == 'editor' || Auth::user()->role == 'admin')
-                    <th>Action</th>
-                @endif
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($employe as $value)
-                <tr>
-                    <td>{{ $value->emp_nss }}</td>
-                    <td>{{ $value->emp_nom }}</td>
-                    <td>{{ $value->emp_prn }}</td>
-                    <td>{{ $value->emp_tarif }}</td>
-                    @if (Auth::user()->role == 'editor' || Auth::user()->role == 'admin')
-                        <td>
-                            <button wire:click="edit({{ json_encode($value->emp_nss) }})"
-                                class="btn btn-primary btn-sm">Edit</button>
-                            @if (Auth::user()->role == 'admin')
-                                <button wire:click="delete({{ json_encode($value->emp_nss) }})"
-                                    class="btn btn-danger btn-sm">Delete</button>
-                            @endif
-                        </td>
-                    @endif
+        <div class="flex flex-col">
+            <div class="w-full">
+                <div class="p-4 border-b border-gray-200 shadow flex justify-center">
+                    <table class="p-4" id="sampleTable">
+                        <thead>
+                            <tr class="whitespace-nowrap">
+                                <th class="p-8 text-xs text-gray-500">Emp_nss</th>
+                                <th class="p-8 text-xs text-gray-500">Emp_nom</th>
+                                <th class="p-8 text-xs text-gray-500">Emp_prn</th>
+                                <th class="p-8 text-xs text-gray-500">Emp_tarif</th>
+                                @if (Auth::user()->role == 'editor' || Auth::user()->role == 'admin')
+                                    <th class="p-8 text-xs text-gray-500">Action</th>
+                                @endif
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white">
+                            @foreach ($employe as $value)
+                                <tr class="whitespace-nowrap">
+                                    <td class="px-6 py-4 text-sm text-center text-gray-500">{{ $value->emp_nss }}</td>
+                                    <td class="px-6 py-4 text-sm text-center text-gray-500">{{ $value->emp_nom }}</td>
+                                    <td class="px-6 py-4 text-sm text-center text-gray-500">{{ $value->emp_prn }}</td>
+                                    <td class="px-6 py-4 text-sm text-center text-gray-500">{{ $value->emp_tarif }}
+                                    </td>
+                                    @if (Auth::user()->role == 'editor' || Auth::user()->role == 'admin')
+                                        <td class="px-6 py-4 text-sm text-center text-gray-500">
+                                            <x-button wire:click="edit({{ json_encode($value->emp_nss) }})">Edit
+                                            </x-button>
+                                            @if (Auth::user()->role == 'admin')
+                                                <x-button wire:click="delete({{ json_encode($value->emp_nss) }})"
+                                                    class="text-sm text-gray bg-red-400 rounded">Delete</x-button>
+                                            @endif
+                                        </td>
+                                    @endif
 
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script>
         $(document).ready(function() {

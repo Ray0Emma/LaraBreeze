4376b49b4af2eceb11ebded882bd975c9245c540
Ray0Emma <ahmadifarah22@gmail.com>
2022-04-24 07:08:15 +0000

    feat: fixed agricultures ui

--- resources/views/livewire/agriculteur/create.blade.php
+++ resources/views/livewire/agriculteur/create.blade.php
@@ -1,25 +1,31 @@
-<form>
-    <div class="form-group">
-        <label for="exampleFormControlInput1">Agriculteur nom</label>
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Title"
-            wire:model="agr_nom">
-        @error('agr_nom')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="exampleFormControlInput2">Agriculteur prenom </label>
-        <input type="text" class="form-control" id="exampleFormControlInput2" wire:model="agr_prn">
-        @error('agr_prn')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="exampleFormControlInput3">Agriculteur Residance</label>
-        <input type="text" class="form-control" id="exampleFormControlInput3" wire:model="agr_resid">
-        @error('agr_resid')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-    </div>
-    <button wire:click.prevent="store()" class="btn btn-success">Save</button>
-</form>
+<x-auth-card>
+    <x-slot name="logo">
+        <a href="/">
+            <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+        </a>
+    </x-slot>
+    <form>
+        <div class="form-group">
+            <x-label for="exampleFormControlInput1">Agriculteur nom</x-label>
+            <x-input class="mt-1 w-full" type="text" id="exampleFormControlInput1" wire:model="agr_nom" />
+            @error('agr_nom')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <x-label for="exampleFormControlInput2">Agriculteur prenom </x-label>
+            <x-input class="mt-1 w-full" type="text" id="exampleFormControlInput2" wire:model="agr_prn" />
+            @error('agr_prn')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <x-label for="exampleFormControlInput3">Agriculteur Residance</x-label>
+            <x-input class="mt-1 w-full" type="text" id="exampleFormControlInput3" wire:model="agr_resid" />
+            @error('agr_resid')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        <x-button wire:click.prevent="store()" class="mt-4">Save</x-button>
+    </form>
+</x-auth-card>
--- resources/views/livewire/agriculteur/update.blade.php
+++ resources/views/livewire/agriculteur/update.blade.php
@@ -1,26 +1,32 @@
-<form>
-    <div class="form-group">
-        <input type="hidden" wire:model="agr_id">
-        <label for="exampleFormControlInput1">Agr_Nom</label>
-        <input type="agr_nom" class="form-control" wire:model="agr_nom" id="exampleFormControlInput1">
-        @error('agr_nom')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="exampleFormControlInput2">Agr_Prenom</label>
-        <input type="text" class="form-control" wire:model="agr_prn" id="exampleFormControlInput2">
-        @error('agr_prn')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="exampleFormControlInput3">Agr_Resid</label>
-        <input type="text" class="form-control" wire:model="agr_resid" id="exampleFormControlInput3">
-        @error('agr_resid')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
-    </div>
-    <button wire:click.prevent="update()" class="btn btn-dark">Update</button>
-    <button wire:click.prevent="cancel()" class="btn btn-danger">Cancel</button>
-</form>
+<x-auth-card>
+    <x-slot name="logo">
+        <a href="/">
+            <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+        </a>
+    </x-slot>
+    <form>
+        <div class="form-group">
+            <x-label for="exampleFormControlInput1">Agriculteur nom</x-label>
+            <x-input class="mt-1 w-full" type="text" id="exampleFormControlInput1" wire:model="agr_nom" />
+            @error('agr_nom')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <x-label for="exampleFormControlInput2">Agriculteur prenom </x-label>
+            <x-input class="mt-1 w-full" type="text" id="exampleFormControlInput2" wire:model="agr_prn" />
+            @error('agr_prn')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <x-label for="exampleFormControlInput3">Agriculteur Residance</x-label>
+            <x-input class="mt-1 w-full" type="text" id="exampleFormControlInput3" wire:model="agr_resid" />
+            @error('agr_resid')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        <x-button wire:click.prevent="update()" class="mt-4">Update</x-button>
+        <x-button wire:click.prevent="cancel()" class="mt-4 text-sm text-gray bg-red-400 rounded">Cancel</x-button>
+    </form>
+</x-auth-card>

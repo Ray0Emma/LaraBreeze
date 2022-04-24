328d71420bc7dd241725cf39435856ebd2103007
Ray0Emma <ahmadifarah22@gmail.com>
2022-04-24 13:05:56 +0000

    feat: added data item

--- app/Http/Livewire/Data.php
+++ app/Http/Livewire/Data.php
@@ -3,13 +3,19 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Agriculteur;
+use App\Models\Intervention;
+use App\Models\Parcelle;
 
 class Data extends Component
 {
+    public $search, $n, $m, $from, $to;
+
+    protected $queryString = ['search', 'from', 'to'];
     public $qst_select;
-    public $qsts = ["Noms des agriculteurs", "Superficie est supérieure à 500", "Parcelles situées à Arith dont la
-    superficie est supérieure à 200 et inférieure à 500", "Parcelles avec le nom de leurs propriétaires", "Interventions effectuées entre le 07-11-2011 et le 09-02-2012", "Chaque intervention et le nom de la parcelle concernée", "Chaque intervention le nom de la parcelle concernée et le nom de
-    l'employé.", "Interventions de l'employe Pernet", "la superficie totale des parcelles", "le nom de la plus grande parcelle", "le nom de la plus petite parcelle"];
+    public $qsts = ["Noms des agriculteurs", "Superficie est supérieure à une valeur", "Parcelles situées à un lieu dont la
+    superficie est supérieure est entre 2 valeur", "Parcelles avec le nom de leurs propriétaires", "Interventions effectuées dans une date", "Chaque intervention et le nom de la parcelle concernée", "Chaque intervention le nom de la parcelle concernée et le nom de
+    l'employé.", "Interventions d'un employe", "la superficie totale des parcelles", "le nom de la plus grande parcelle", "le nom de la plus petite parcelle"];
     public $question1;
     public $question2;
     public $question3;
@@ -24,6 +30,24 @@ class Data extends Component
 
     public function render()
     {
+        $this->question1 = Agriculteur::all("agr_nom")->sortBy("agr_nom");
+        $this->question2 = Parcelle::where("par_superficie", ">", "" . $this->search . "")->get();
+        $this->question3 = Parcelle::where("emp_lieu", "" . $this->search . "", "and")->whereBetween("par_superficie", [$this->from, $this->to])->get();
+        $this->question4 = Parcelle::join('agriculteurs', 'parcelles.par_prop', '=', 'par_id')->get(['parcelles.*', 'agriculteurs.agr_nom']);
+
+        $this->question5 = Intervention::whereBetween("int_debut", [$this->from, $this->to])->get();
+        $this->question6 = Intervention::join('parcelles', 'interventions.int_par_id', '=', 'parcelles.par_id')->get(['parcelles.par_nom', 'interventions.*']);
+        $this->question7 = Intervention::join('employes', 'interventions.int_emp_nss', '=', 'employes.emp_nss')
+            ->join('parcelles', 'interventions.int_par_id', '=', 'parcelles.par_id')
+            ->get(['employes.emp_nom', 'parcelles.par_nom', 'interventions.*']);
+        $this->question8 = Intervention::join('employes', 'interventions.int_emp_nss', '=', 'employes.emp_nss')
+            ->select('employes.emp_nom', 'interventions.*')
+            ->where('employes.emp_nom', "" . $this->search . "")
+            ->get();
+
+        $this->question9 = Parcelle::sum('par_superficie');
+        $this->question10 =  Parcelle::select('par_nom')->orderBy('par_superficie', 'asc')->first();
+        $this->question11 = Parcelle::select('par_nom')->orderBy('par_superficie', 'desc')->first();
         return view('livewire.data');
     }
 }
--- resources/views/livewire/data.blade.php
+++ resources/views/livewire/data.blade.php
@@ -22,63 +22,161 @@
                 <div class="mt-28 p-4 border-b border-gray-200 shadow ">
                     @switch($qst_select)
                         @case('0')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="flex justify-center p-4">
+                                <h1>Search Results:</h1>
+                                <table class="flex justify-center">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Nom</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($question1 as $question)
+                                            <tr>
+                                                <th>{{ $question['agr_nom'] }}</th>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         @break
 
                         @case('1')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="p-4">
+                                <input wire:model="search" type="search" placeholder="Search by...">
+
+                                <h1>Search Results:</h1>
+
+                                <ul class="flex justify-center">
+                                    @foreach ($question2 as $quest2)
+                                        <li>{{ $quest2['par_nom'] }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         @break
 
                         @case('2')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="p-4">
+                                <input wire:model="search" type="search" placeholder="Search by...">
+                                <input wire:model="from" type="search" placeholder="Search by...">
+                                <input wire:model="to" type="search" placeholder="Search by...">
+
+                                <h1>Search Results:</h1>
+
+                                <ul class="flex justify-center">
+                                    @foreach ($question3 as $question)
+                                        <li><strong>Nom:</strong> {{ $question['par_nom'] }} <strong>Lieu:</strong>
+                                            {{ $question['emp_lieu'] }} <strong>Superficie:</strong>
+                                            {{ $question['par_superficie'] }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         @break
 
                         @case('3')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="flex justify-center p-4">
+                                <h1>Search Results:</h1>
+                                <ul>
+                                    @foreach ($question4 as $quest4)
+                                        <li><strong>Parcelles: </strong>{{ $quest4['par_nom'] }} <strong>Nom Propriétaire:
+                                            </strong> {{ $quest4['agr_nom'] }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         @break
 
                         @case('4')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="p-4">
+                                <input wire:model="from" type="search" placeholder="Search by...">
+                                <input wire:model="to" type="search" placeholder="Search by...">
+
+                                <h1>Search Results:</h1>
+                                <ul>
+                                    @foreach ($question5 as $quest5)
+                                        <li> <strong>Int debut : </strong>{{ $quest5['int_debut'] }}</li>
+                                        <li> <strong>Nombre de jours : </strong>{{ $quest5['int_nb_jrs'] }}</li><br>
+                                    @endforeach
+                                </ul>
+                            </div>
                         @break
 
                         @case('5')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="flex justify-center p-4">
+                                <h1>Search Results:</h1>
+                                <ul>
+                                    @foreach ($question6 as $quest6)
+                                        <li><strong>Debut d'intervention :</strong> {{ $quest6['int_debut'] }} <strong>le nom
+                                                de
+                                                la
+                                                parcelle concernée :</strong> {{ $quest6['par_nom'] }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         @break
 
                         @case('6')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="flex justify-center p-4">
+                                <h1>Search Results:</h1>
+                                <ul>
+                                    @foreach ($question7 as $quest7)
+                                        <li><strong>Debut d'intervention</strong> : {{ $quest7['int_debut'] }}
+                                            <strong>Le nom de la parcelle concernée:</strong> {{ $quest7['par_nom'] }}
+                                            <strong>Le nom de l'employé :</strong> {{ $quest7['emp_nom'] }}
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         @break
 
                         @case('7')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="p-4">
+                                <input wire:model="search" type="search" placeholder="Search by...">
+
+                                <h1>Search Results:</h1>
+                                <ul>
+                                    @foreach ($question8 as $quest8)
+                                        <li><strong>Interventions de l'employe
+                                            </strong>{{ $search }} : {{ $quest8['int_emp_nss'] }}
+                                            <strong>Debut d'intervention</strong> : {{ $quest8['int_debut'] }}
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         @break
 
                         @case('8')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="flex justify-center p-4">
+                                <h1>Search Results:</h1>
+                                <ul>
+
+                                    <li><strong>La superficie totale des parcelles :{{ $question9 }} </strong></li>
+
+                                </ul>
+                            </div>
                         @break
 
                         @case('9')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                            <div class="flex justify-center p-4">
+                                <h1>Search Results:</h1>
+                                <ul>
+
+                                    <li><strong>Le nom de la plus grande parcelle : </strong>{{ $question10->par_nom }}
+                                    </li>
+
+                                </ul>
+                            </div>
                         @break
 
                         @case('10')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
-                        @break
+                            <div class="flex justify-center p-4">
+                                <h1>Search Results:</h1>
+                                <ul>
 
-                        @case('11')
-                            {{ $qst_select }}
-                            @include('livewire.data.qst1')
+                                    <li><strong>Le nom de la plus petite parcelle :</strong> {{ $question11->par_nom }}
+                                    </li>
+
+                                </ul>
+                            </div>
                         @break
 
                         @default
